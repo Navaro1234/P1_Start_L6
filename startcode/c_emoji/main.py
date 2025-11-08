4e05a1ec28995525794d606c8e721c36439083e1
Navaro Devos <164686546+Navaro1234@users.noreply.github.com>
2025-11-08 16:19:00 +0100

    oefening opgelost

---
+++ startcode/c_emoji/main.py
@@ -0,0 +1,6 @@
+from mijn_emoji import *
+
+print(emoji_lachend_gezichtje())
+print(emoji_hartje())
+print(aap())
+print(emoji_hartje() + emoji_lachend_gezichtje() + aap())
---
+++ startcode/c_emoji/mijn_emoji.py
@@ -0,0 +1,7 @@
+def emoji_lachend_gezichtje():
+    return "😊"
+
+def emoji_hartje():
+    return "❤️"
+def aap():
+    return "🙉"
