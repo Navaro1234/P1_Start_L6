aaf9a408054138310013bee90cfee9b3740dfe57
Navar <navarokeen@outlook.be>
2025-11-08 15:26:07 +0100

    oefening opgelost

--- startcode/0_debug/debug.py
+++ startcode/0_debug/debug.py
@@ -1,16 +1,19 @@
+import turtle
+
 aantal_punten = input("Hoeveel punten moet de ster hebben? ")
+aantal_punten = int(aantal_punten)
 
-t = turtle.Turltle()
-t.speed(4)
+t = turtle.Turtle()
+t.speed(100)
 
-for i in range(aantal_punten)
+for i in range(aantal_punten):
     if i % 2 == 0:
         t.color("red")
     else:
-    t.color("blue")
+     t.color("blue")
 
     t.forward(200)
-    t.right(180 - 180 / aantalpunten)
+    t.right(180 - 180 / aantal_punten)
 
 t.hideturtle()
 turtle.done()
